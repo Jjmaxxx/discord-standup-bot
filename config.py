51c1938dd25f60500e0ffea9c9a1c1217b007352
Justin <justlee888@gmail.com>
2024-11-02 16:45:11 -0700

    removed token print

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('bot_token')
-print(f'Token: {TOKEN}')
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
