e9d515195acfa5ecb4e204ab2c820ceb13b1d974
Chance <ctkiscool13@gmail.com>
2024-11-05 20:21:04 -0800

    Embeds used for each command

--- cogs/roles_commands.py
+++ cogs/roles_commands.py
@@ -15,7 +15,13 @@ class roles_commands(commands.Cog):
         }
         channel_name = role.name
         channel = await guild.create_text_channel(channel_name, overwrites=overwrites, category=category)
-        await ctx.send(f'"{role.name}" channel has been created')
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            title="Success!",
+            description=f'"{role.name}" channel has been created'
+        )
+        embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
+        await ctx.send(embed=embed)
         
     @commands.command()
     async def createRole(self, ctx, roleName: str):
@@ -25,12 +31,24 @@ class roles_commands(commands.Cog):
         
         #Run check to see if the role name has already been used
         if discord.utils.get(guild.roles, name = roleName):
-            await ctx.send(f'The role "{roleName}" has already been created.')
+            embed = discord.Embed(
+                color=discord.Color.blurple(),
+                title="Sorry!",
+                description=f'The role "{roleName}" has already been created'
+            )
+            embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
+            await ctx.send(embed=embed)
             return
         
         #If the name has not been used, create the role
         role = await guild.create_role(name = f"{config.GROUP_PREFIX}{roleName}")
-        await ctx.send(f'"{roleName}" has been created.')
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            title="Success!",
+            description=f'"{roleName}" has been created.'
+        )
+        embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
+        await ctx.send(embed=embed)
         await self.createRoleChannel(ctx,guild,role)
     
     @commands.command()
@@ -39,21 +57,49 @@ class roles_commands(commands.Cog):
         matching_roles = [role for role in guild.roles if role.name.startswith(config.GROUP_PREFIX)]
         if matching_roles:
             message = "\n".join(role.name[len(config.GROUP_PREFIX):] for role in matching_roles)
+            message2 = "Success!"
         else:
-            message = "No roles found starting with the specified prefix."        
-        await ctx.send(message)
+            message = "No roles found starting with the specified prefix."
+            message2 = "Sorry!"
+
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            title=message2,
+            description=message
+        )
+        embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")      
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def addUserRole(self, ctx, role_name):
         guild = ctx.guild
         role = discord.utils.get(guild.roles, name=f"{config.GROUP_PREFIX}{role_name}")
         if role is None:
-            await ctx.send(f'Role "{role_name}" does not exist.')
+            embed = discord.Embed(
+                color=discord.Color.blurple(),
+                title="Sorry!",
+                description=f'Role "{role_name}" does not exist.'
+            )
+            embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
+            await ctx.send(embed=embed)
             return
         if role in ctx.author.roles:
-            await ctx.send(f'You already have the role "{role_name}".')
+            embed = discord.Embed(
+                color=discord.Color.blurple(),
+                title="Sorry!",
+                description=f'You already have the role "{role_name}".'
+            )
+            embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
+            await ctx.send(embed=embed)
             return
+        embed = discord.Embed(
+                color=discord.Color.blurple(),
+                title="Success!",
+                description=f'Role "{role_name}" has been added to you!'
+            )
+        embed.set_thumbnail(url="https://seeklogo.com/images/S/san-jose-state-spartans-logo-E3E560A879-seeklogo.com.png")
         await ctx.author.add_roles(role)
-        await ctx.send(f'Role "{role_name}" has been added to you!')
+        await ctx.send(embed=embed)
 
         
 
